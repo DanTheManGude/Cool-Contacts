88dd0a3b00538704ea3559e6cf8de4cdd1ff15d1
DanTheManGude <djg4755@rit.edu>
2018-01-15 13:06:58 -0500

    added comments

--- js/index.js
+++ js/index.js
@@ -1,22 +1,21 @@
 
 function preview() {
+    //update elements in the SVG with new values
     $("#initials").text($("#it").val());
     $(".cir").css("fill", $("#colur").val())
 }
 
 function submit() {
+    //convert SVG to image and download
     saveSvgAsPng(document.getElementById("diagram"), "Cool-Contact-" + $("#it").val() + ".jpg");
 }
 
 function randColor() {
+    //get a new random color
     newColor = randomColor( {luminosity: 'dark'} );
+    //update the color picker with new values
     $("#colur").val(newColor);
     $("#colur").css("background-color", newColor);
+    //update the contact with new value
     preview();
-}
-
-function newEntry(newInitials) {
-    $("#it").val(newInitials);
-    randColor()
-    submit();
 }
